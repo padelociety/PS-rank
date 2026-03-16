8f4b85ab12305096cbaf23b45cf724549ae242b0
padelociety <padelociety@gmail.com>
2026-03-16 10:10:15 +0900

    different app

---
+++ ps_court_sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'ps-court-v1';
+const ASSETS = [
+  '/PS-rank/ps_court.html',
+  '/PS-rank/ps_court_manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
